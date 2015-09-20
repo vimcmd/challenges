9f9f6fb7921939ce2f0e7b5ca45df240c0679c4c
vimcmd <vim.cmd@gmail.com>
2015-09-20 22:50:05 +0500

    initial commit

---
+++ src/com/programmr/java/Challenges/Miscellaneous/PalindromeString/PalindromeString.java
@@ -0,0 +1,41 @@
+package com.programmr.java.Challenges.Miscellaneous.PalindromeString;
+
+import java.io.*;
+
+public class PalindromeString
+{
+
+    static String inputString;
+    static String reversedString;
+
+    public static String reverseString(String input) {
+        input = removeSymbols(input);
+        return new StringBuilder(input).reverse().toString().toLowerCase();
+    }
+
+    public static boolean isPalindrome(String input) {
+        input = removeSymbols(input);
+        return (input.toLowerCase().equals(reversedString));
+    }
+
+    public static String removeSymbols(String str) {
+        return str.replaceAll("[.><,:;!/@#$%^&*()-=_+']","");
+    }
+
+    public static void main(String a[]) throws Exception
+    {
+        System.out.println("Enter the string:");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        inputString = br.readLine();
+        System.out.println("Result string is:");
+        reversedString = reverseString(inputString);
+        System.out.println(reversedString);
+
+        if(isPalindrome(inputString)) {
+            System.out.print("palindrome");
+        }
+        else {
+            System.out.print("not a palindrome");
+        }
+    }
+}
---
+++ tests/com/programmr/java/Challenges/Miscellaneous/PalindromeString/PalindromeStringTest.java
@@ -0,0 +1,36 @@
+package com.programmr.java.Challenges.Miscellaneous.PalindromeString;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class PalindromeStringTest {
+
+    @Test
+    public void testMain() throws Exception {
+
+
+    }
+
+    @Test
+    public void testReverseString() throws Exception {
+        final String string = "world";
+        final String stringReversed = "dlrow";
+        assertEquals(stringReversed, PalindromeString.reverseString(string));
+    }
+
+    @Test
+    public void testIsPalindrome() throws Exception {
+        final String palindromeString = "Sum summus mus".toLowerCase();
+        assertEquals(palindromeString, PalindromeString.reverseString(palindromeString));
+    }
+
+    @Test
+    public void testRemoveSymbols() throws Exception {
+        final String stringWithSymbols = ".><w,o:r;l!d/@#$%^&*()-=_+'";
+        final String string = "world";
+        assertEquals(string, PalindromeString.removeSymbols(stringWithSymbols));
+    }
+
+}
