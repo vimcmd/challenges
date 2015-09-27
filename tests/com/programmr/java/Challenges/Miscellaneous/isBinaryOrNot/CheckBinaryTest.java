81741f62edeaf29250cf8ebd766ed2517caab846
vimcmd <vim.cmd@gmail.com>
2015-09-27 14:51:43 +0500

    check binary tests init

---
+++ tests/com/programmr/java/Challenges/Miscellaneous/isBinaryOrNot/CheckBinaryTest.java
@@ -0,0 +1,21 @@
+package com.programmr.java.Challenges.Miscellaneous.isBinaryOrNot;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class CheckBinaryTest {
+
+    // TODO: how to write JUnit4 test for user console input/output
+
+    @Test
+    public void testMain() throws Exception {
+
+    }
+
+    @Test
+    public void testCheckBinary() throws Exception {
+
+    }
+}
