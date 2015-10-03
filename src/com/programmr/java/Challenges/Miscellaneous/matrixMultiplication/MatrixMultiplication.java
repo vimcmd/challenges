1bb4f09bda07843ee8847fa079d6d5898d6109af
vimcmd <vim.cmd@gmail.com>
2015-10-03 13:38:37 +0500

    Matrix multiplication readme added

--- src/com/programmr/java/Challenges/Miscellaneous/matrixMultiplication/MatrixMultiplication.java
+++ src/com/programmr/java/Challenges/Miscellaneous/matrixMultiplication/MatrixMultiplication.java
@@ -4,45 +4,6 @@ package com.programmr.java.Challenges.Miscellaneous.matrixMultiplication;
 import java.util.*;
 public class MatrixMultiplication{
 
-/*
-
-Write a program to takes two matrix(3*3) array as inputs and calculate the multiplication of both matrix.
-
-Example :
-1. If two matrix are :
-Matrix 1st :
-1 2 3
-4 5 6
-7 8 1
-
-Matrix 2nd:
-1 2 3
-3 2 1
-4 5 2
-
-then the multiplication of both matrix should be :
-19 21 11
-43 48 29
-35 35 31
-
-2. If two matrix are :
-Matrix 1st :
-4 4 4
-2 2 2
-3 3 3
-
-Matrix 2nd :
-1 4 3
-3 2 4
-1 2 4
-
-then the multiplication of both matrix should be :
-20 32 44
-10 16 22
-15 24 33
-
-*/
-
     public static int[][] matrixMultiplication(final int[][] arr1, final int[][] arr2) {
         int result[][] = new int[3][3];
 
