efcec03e91e1c259004059d8cf987b6ac432f213
vimcmd <vim.cmd@gmail.com>
2015-10-03 13:58:46 +0500

    checkBinary readme added

--- src/com/programmr/java/Challenges/Miscellaneous/isBinaryOrNot/CheckBinary.java
+++ src/com/programmr/java/Challenges/Miscellaneous/isBinaryOrNot/CheckBinary.java
@@ -3,21 +3,6 @@ package com.programmr.java.Challenges.Miscellaneous.isBinaryOrNot;
 import java.io.*;
 import java.util.Scanner;
 
-/*
-A binary number is written as combination of 1's and 0's.
-Binary number 100 indicates 4.
-
-Write a program which accepts a number and prints whether it is binary or not.
-
-For Ex:
-
-1) if user inputs 1040400
-then it should print "Not Binary".
-
-2) if user inputs 1000100
-then it should print "Binary".
-*/
-
 public class CheckBinary {
 
     public static void checkBinary(final int num) {
---
+++ src/com/programmr/java/Challenges/Miscellaneous/isBinaryOrNot/readme.md
@@ -0,0 +1,13 @@
+# Check binary
+A binary number is written as combination of 1's and 0's.
+Binary number 100 indicates 4.
+
+Write a program which accepts a number and prints whether it is binary or not.
+
+### Example:
+
+1) if user inputs 1040400
+then it should print "Not Binary".
+
+2) if user inputs 1000100
+then it should print "Binary".
