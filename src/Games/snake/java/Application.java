77d848d3f12571e3b020e42c904e1e9c5cffc094
vimcmd <vim.cmd@gmail.com>
2015-10-25 04:24:27 +0500

    added Esc key listener

--- src/Games/snake/java/Application.java
+++ src/Games/snake/java/Application.java
@@ -1,8 +1,10 @@
 package Games.snake.java;
 
-import com.googlecode.lanterna.TerminalFacade;
-import com.googlecode.lanterna.terminal.*;
 import java.nio.charset.Charset;
+
+import com.googlecode.lanterna.TerminalFacade;
+import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.input.Key;
 
 
 public class Application {
@@ -10,6 +12,18 @@ public class Application {
     public static void main(String[] args) {
         Terminal terminal = TerminalFacade.createTerminal(System.in, System.out, Charset.forName("UTF8"));
         terminal.enterPrivateMode();
+
+
+        // exit terminal on press Esc key
+        while(true) {
+            Key key = terminal.readInput();
+            if (key != null) {
+                if (key.getKind() == Key.Kind.Escape) {
+                    System.exit(0);
+                }
+            }
+        }
+
     }
 
 }
