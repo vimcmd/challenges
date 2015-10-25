0ff34d9b395c8ed5ae2fbb350002de710b8173ef
vimcmd <vim.cmd@gmail.com>
2015-10-25 19:11:19 +0500

    initBorders method added

--- src/Games/snake/java/Application.java
+++ src/Games/snake/java/Application.java
@@ -3,23 +3,23 @@ package Games.snake.java;
 import java.nio.charset.Charset;
 
 import com.googlecode.lanterna.TerminalFacade;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.ScreenCharacterStyle;
 import com.googlecode.lanterna.terminal.Terminal;
 import com.googlecode.lanterna.input.Key;
+import com.googlecode.lanterna.terminal.TerminalSize;
 
 public class Application {
 
     public static void main(String[] args) {
         Terminal terminal = TerminalFacade.createTerminal(System.in, System.out, Charset.forName("UTF8"));
-        terminal.enterPrivateMode();
+        // TerminalSize screenSize = terminal.getTerminalSize();
+        Screen screen = TerminalFacade.createScreen(terminal);
+        screen.startScreen();
+        screen.putString(0, 0, "Press Esc to exit", Terminal.Color.WHITE, Terminal.Color.BLACK);
+        screen.refresh();
 
-        Point p1 = new Point(5, 6);
-        p1.Draw(terminal);
-
-        Point p2 = new Point(3, 4, '#');
-        p2.Draw(terminal);
-
-//        Draw(terminal, p1);
-//        Draw(terminal, new Point(4, 3));
+        initBorders(terminal);
 
 //         exit terminal on press Esc key
 //         TODO add window.close listener
@@ -32,6 +32,21 @@ public class Application {
                 }
             }
         }
+
+    }
+
+    public static void initBorders(Terminal terminal) {
+        Obstacle borderTop = new Obstacle( 0, 1, '%', true, terminal.getTerminalSize().getColumns() - 1 );
+        borderTop.Draw(terminal);
+
+        Obstacle borderBottom = new Obstacle( 0, terminal.getTerminalSize().getRows() - 1, '%', true, terminal.getTerminalSize().getColumns() - 1 );
+        borderBottom.Draw(terminal);
+
+        Obstacle borderLeft = new Obstacle(0, 1, '%', false, terminal.getTerminalSize().getRows() - 1);
+        borderLeft.Draw(terminal);
+
+        Obstacle borderRight = new Obstacle(terminal.getTerminalSize().getColumns() - 1, 1, '%', false, terminal.getTerminalSize().getRows() - 1 );
+        borderRight.Draw(terminal);
 
     }
 
