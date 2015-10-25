6bd9cb68a7b9c351fd12d048867fbe7dd6b3017d
vimcmd <vim.cmd@gmail.com>
2015-10-25 16:50:29 +0500

    Point class added

---
+++ src/Games/snake/java/Point.java
@@ -0,0 +1,27 @@
+package Games.snake.java;
+
+import com.googlecode.lanterna.terminal.Terminal;
+
+// TODO can be extended from java.awt.Point
+public class Point {
+    private int x;
+    private int y;
+    private char ch;
+
+    public Point(int x, int y, char ch) {
+        this.x = x;
+        this.y = y;
+        this.ch = ch;
+    }
+
+    public Point(int x, int y) {
+        this(x, y, '*');
+    }
+
+    public void Draw(Terminal terminal) {
+        terminal.moveCursor(this.x, this.y);
+        terminal.putCharacter(this.ch);
+        terminal.moveCursor(0, 0);
+    }
+
+}
