39ac5720f7e61194dfeb422188866c5e4d9f7397
vimcmd <vim.cmd@gmail.com>
2015-10-31 02:08:39 +0500

    ArrayList replaced with LinkedList due to the presence of getLast() and getFirst() methods

--- src/Games/snake/java/Figure.java
+++ src/Games/snake/java/Figure.java
@@ -2,11 +2,13 @@ package Games.snake.java;
 
 import com.googlecode.lanterna.terminal.Terminal;
 
+import java.util.LinkedList;
 import java.util.List;
 
 
 public class Figure {
-    protected List<Point> pointList;
+    // LinkedList used due to the presence of getLast() and getFirst() methods
+    protected LinkedList<Point> pointList;
 
     public void Draw(Terminal terminal) {
         for (Point p : this.pointList) {
