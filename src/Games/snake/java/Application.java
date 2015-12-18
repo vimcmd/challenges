a3c09c9ff83b5f2a4bde9ca3b1a2ef5149d7296a
vim.cmd <vim.cmd@gmail.com>
2015-12-18 16:12:16 +0500

    set window resizable to false, and make cursor invisible

--- src/Games/snake/java/Application.java
+++ src/Games/snake/java/Application.java
@@ -7,7 +7,6 @@ import com.googlecode.lanterna.terminal.swing.SwingTerminal;
 public class Application
 {
     // TODO: move to main() with terminal size as args
-    // Create terminal window with listener to close (X) button
     final static SwingTerminal terminal = new SwingTerminal(120, 30);
     final static Screen screen = new Screen(terminal);
 
@@ -15,6 +14,8 @@ public class Application
     {
 
         screen.startScreen();
+        terminal.getJFrame().setLocationRelativeTo(null); // this will center JFrame window
+        terminal.getJFrame().setResizable(false);
         terminal.getJFrame().addWindowListener(new java.awt.event.WindowAdapter()
                                                {
                                                    public void windowClosing(java.awt.event.WindowEvent event)
@@ -25,19 +26,21 @@ public class Application
                                                }
         );
 
-        Obstacle.initBorders(terminal);
-
         Snake snake = new Snake(new Point(1, 1), 15);
 
         while (true)
         {
-            Key key = terminal.readInput();
-            snake.handleControl(key);
-            snake.move();
-            snake.draw(terminal);
-            screen.refresh();
+            Obstacle.drawBorders(terminal);
 
-            Thread.sleep(100);
+            Key key = terminal.readInput();
+            snake.move(key);
+            snake.draw(terminal);
+
+            {
+                screen.refresh();
+                terminal.setCursorVisible(false);
+                Thread.sleep(100);
+            }
         }
     }
 
