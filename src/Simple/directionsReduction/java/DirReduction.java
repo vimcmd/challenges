ea2eaba18614baeea325d6189962377fff273155
vim.cmd <vim.cmd@gmail.com>
2016-07-20 13:59:05 +0500

    directionsReduction added (not implemented yet)

---
+++ src/Simple/directionsReduction/java/DirReduction.java
@@ -0,0 +1,8 @@
+package Simple.directionsReduction.java;
+
+public class DirReduction {
+    public static String[] dirReduc(String[] arr) {
+        // TODO: 20.07.2016 implement method!
+        return new String[] {};
+    }
+}
---
+++ tests/Simple/directionsReduction/java/DirReductionTest.java
@@ -0,0 +1,20 @@
+package Simple.directionsReduction.java;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DirReductionTest {
+
+    @Test
+    public void testSimpleDirReduc() throws Exception {
+        assertArrayEquals("\"NORTH\", \"SOUTH\", \"SOUTH\", \"EAST\", \"WEST\", \"NORTH\", \"WEST\"",
+                new String[]{"WEST"},
+                DirReduction.dirReduc(new String[]{"NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"}));
+
+        assertArrayEquals("\"NORTH\", \"WEST\", \"SOUTH\", \"EAST\"",
+                new String[]{"NORTH", "WEST", "SOUTH", "EAST"},
+                DirReduction.dirReduc(new String[]{"NORTH", "WEST", "SOUTH", "EAST"}));
+    }
+
+}
